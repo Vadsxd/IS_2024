7b7b602d7c1f3a3406af4901abcab0a995b1f1b6
vadim <vadim.sannikov.2018@mail.ru>
2024-02-23 19:06:37 +0300

    add players

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ const Socket = require('./socket');
 const VERSION = 7;
 
 (async () => {
-    let agent = new Agent();
-    await Socket(agent, 'A', VERSION);
-    await agent.socketSend('move', '-30 0')
+    let playerCords1, playerCords2, rotationSpeed;
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    const it = rl[Symbol.asyncIterator]();
+
+    console.log('Enter first player coords (x,y):');
+    playerCords1 = (await it.next()).value.split(' ').map((a) => +a);
+    console.log('Enter second player coords (x y):');
+    playerCords2 = (await it.next()).value.split(' ').map((a) => +a);
+    console.log('Enter first player rotation speed:');
+    rotationSpeed = +(await it.next()).value;
+    rl.close();
+
+    console.log([playerCords1, playerCords2, rotationSpeed]);
+
+    let player1 = new Agent();
+    let player2 = new Agent();
+
+    await Socket(player1, 'A', VERSION);
+    await Socket(player2, 'B', VERSION);
+
+    await player1.socketSend('move', `${playerCords1[0]} ${playerCords1[1]}`)
+    await player2.socketSend('move', `${playerCords2[0]} ${playerCords2[1]}`)
 })();
