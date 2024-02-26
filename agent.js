f3034204446200d93109e83b30ff0a540b6363ff
vasiliy2002 <71724805+vasiliy2002@users.noreply.github.com>
2024-02-26 14:16:23 +0300

    done

--- agent.js
+++ agent.js
@@ -3,34 +3,28 @@ const readline = require('readline');
 const utils = require("./utils");
 
 class Agent {
-    constructor() {
+    constructor(teamName) {
         this.position = 'l'; // По умолчанию - левая половина поля
         this.run = false; // Игра начата
         this.act = null; // Действия
         this.rotationSpeed = null; // скорость вращения
         this.x_boundary = 57.5;
         this.y_boundary = 39;
-        /*
-        this.rl = readline.createInterface({
-            // Чтение консоли
-            input: process.stdin,
-            output: process.stdout,
-        });
-        this.posMethod = '3P';
+        this.teamName = teamName;
+        this.DirectionOfSpeed = null;
 
-        this.rl.on('line', (input) => {
-            // Обработка строки из кон—
-            if (this.run) {
-                // Если игра начата
-                // ДВижения вперед, вправо, влево, удар по мячу
+    }
 
-                if ('w' === input) this.act = {n: 'dash', v: 100};
-                if ('d' === input) this.act = {n: 'turn', v: 20};
-                if ('a' === input) this.act = {n: 'turn', v: -20};
-                if ('s' === input) this.act = {n: 'kick', v: 100};
-            }
-        });
-        */
+    get_unit_vector(Direction){
+        if (!this.DirectionOfSpeed){
+            return;
+        }
+        if (this.teamName === 'A'){
+            var angle = this.DirectionOfSpeed -  Direction;
+            angle = angle * Math.PI / 180;
+
+            return [Math.cos(angle), -Math.sin(angle)];
+        }
     }
 
     msgGot(msg) {
@@ -66,48 +60,68 @@ class Agent {
     }
 
     analyzeEnv(msg, cmd, p) {
-        // анализ сообщения
-        //this.act = {n: 'turn', v: 20};
-        /*
-        console.log("message: ", msg);
-        console.log("cmd: ", cmd)
-        console.log("p: ", p)
-        if (cmd === "see"){
-            console.log("Array: ", p[1]["cmd"]["p"])
-        }
-        */
-
-        
         if (this.rotationSpeed){
-            this.act = {n: 'turn', v: this.rotationSpeed};
+            if (!this.act){
+                this.act = {n: 'turn', v: this.rotationSpeed};
+            }
+        } else {
+            return;
         }
 
-        
-        var flag1 = null;
-        var flag2 = null;
-        //flags_params = null;
+        if (cmd === "sense_body"){
+            this.DirectionOfSpeed = p[3]['p'][1];
+        }
+
+
         if (cmd === "see"){
-            var flags = utils.get_flags(p);
-            console.log(flags);
-            //console.log(flags);
-            
+            let flag1 = null;
+            let flag2 = null;
+            let flag3 = null;
+            let coordinates;
+            let flags_and_objects = utils.get_flags_and_objects_2(p);
+            let flags = flags_and_objects[0];
+            let objects = flags_and_objects[1];
+
             if (flags.length === 2){
                 flag1 = flags[0];
                 flag2 = flags[1];
-                var coordinates = utils.solveby2(flag1[2], flag2[2], flag1[0], flag1[1], flag2[0], flag2[1],
-                    flag1[3], flag2[3], this.x_boundary, this.y_boundary);
-                console.log(coordinates);
+                let e1 = this.get_unit_vector(flag1[3]);
+                let e2 = this.get_unit_vector(flag2[3]);
+
+                let object;
+                let obj_coords;
+                coordinates = utils.solveby2(flag1[2], flag2[2], flag1[0], flag1[1], flag2[0], flag2[1],
+                    e1, e2, this.x_boundary, this.y_boundary);
+                if (coordinates){
+                    console.log('coordinates:', coordinates);   
+                }
+                
             }
 
             if (flags.length === 3){
-                // вызов функции utils.solveby3 с получением координат игрока и печатью в консоль.
+                flag1 = flags[0];
+                flag2 = flags[1];
+                flag3 = flags[2];
+                coordinates = utils.solveby3(flag1[2], flag2[2], flag3[2], flag1[0], flag1[1],
+                    flag2[0], flag2[1], flag3[0], flag3[1]);
+                if (!isNaN(coordinates[0]) && !isNaN(coordinates[0]) && coordinates[1] !== -Infinity) {
+                    console.log('coordinates:', coordinates);
+                }
             }
 
-            
-            //flags_params.push(this.x_boundary, this.y_boundary);
+            if (objects.length > 0){
+                let object = objects[0];
+                let eo = this.get_unit_vector(object[1]);
+
+                let obj_coords = utils.get_object_coords(flag1[2], object[0], coordinates[0], coordinates[1], flag1[0], flag1[1], flag1[3], object[1], eo);
+                if (obj_coords){
+                    console.log("obj_coords:", obj_coords);    
+                }        
+            }            
         }
-        
     }
+        
+    
 
     sendCmd() {
         if (this.run) {
--- utils.js
+++ utils.js
@@ -5,61 +5,97 @@ module.exports = {
         return x1 * x1 - x2 * x2
     },
 
-    solveby3(d1, d2, d3, x1, y1, x2, y2, x3, y3){
-    	// функция для нахождения координат игрока по трем флагам
-    	// x1, x2, x3 - разные
-    	// возвращает координаты игрока в виде [x, y]
-    }
+    solveby3(d1, d2, d3, x1, y1, x2, y2, x3, y3) {
+        let alpha1 = (y1 - y2) / (x2 - x1);
+        let beta1 = (y2 * y2 - y1 * y1 + x2 * x2 - x1 * x1 + d1 * d1 - d2 * d2) / (2 * (x2 - x1));
+        let alpha2 = (y1 - y3) / (x3 - x1);
+        let beta2 = (y3 * y3 - y1 * y1 + x3 * x3 - x1 * x1 + d1 * d1 - d3 * d3) / (2 * (x3 - x1));
+        let delta_beta = beta1 - beta2;
+        let delta_alpha = alpha2 - alpha1;
+        let X = alpha1 * (delta_beta / delta_alpha) + beta1;
+        let Y = delta_beta / delta_alpha;
 
-    solveby2(d1, d2, x1, y1, x2, y2, x_bound, y_bound){
-        var x, y;
+        return [X, Y];
+    },
+
+    get_similarity(coord, x1, y1, x2, y2, e1, e2){
+    	let vector1 = [x1 - coord[0], y1 - coord[1]];
+    	let vector2 = [x2 - coord[0], y2 - coord[1]];
+    	let result = (e1[0] * vector1[0] + e1[1] * vector1[1]);
+        if (e2){
+            result += e2[0] * vector2[0] + e2[1] * vector2[1];
+        } 
+        return result;
+    },
+
+    get_best(coord1, coord2, x1, y1, x2, y2, e1, e2){
+    	let sim1 = this.get_similarity(coord1, x1, y1, x2, y2, e1, e2);
+    	let sim2 = this.get_similarity(coord2, x1, y1, x2, y2, e1, e2);
+
+    	if (sim1 > sim2){
+    		return coord1;
+    	} 
+    	return coord2;
+    },
+
+    solveby2(d1, d2, x1, y1, x2, y2, e1, e2, x_bound, y_bound){
+        let x, y, x_, y_;
+        let possible_poses = [];
+        let in_field = [];
+        let result = [];
         if (x1 == x2){
             y = (this.squares_diff(y2, y1) + this.squares_diff(d1, d2)) / (2 * (y2 - y1));
             diff = Math.pow(this.squares_diff(d1, y-y1), 0.5);
             x = x1 + diff;
-            if (Math.abs(x) > x_bound){
-                x -= 2 * diff;
-            }
-            return [x, y];
-        }
-  
-        if (y1 == y2){
+            x_ = x - 2 * diff;
+            
+            possible_poses.push([x, y]);
+            possible_poses.push([x_, y]);
+        } else if (y1 == y2){
             x = (this.squares_diff(x2, x1) + this.squares_diff(d1, d2)) / (2 * (x2 - x1));
             diff = Math.pow(this.squares_diff(d1, x-x1), 0.5);
             y = y1 + diff;
-            if (Math.abs(y) > y_bound){
-                y -= 2 * diff;
-            }
-            return [x, y];
+            y_ = y - 2 * diff;
+            
+            possible_poses.push([x, y]);
+            possible_poses.push([x, y_]);
+        } else {
+	        let alpha = (y1 - y2) / (x2 - x1);
+	        let beta = (this.squares_diff(y2, y1) + this.squares_diff(x2, x1) + this.squares_diff(d1, d2)) / (2 * (x2 - x1));
+
+	        let a = alpha * alpha + 1;
+	        let b = -2 * (alpha * (x1 - beta) + y1);
+	        let c = Math.pow(x1 - beta, 2) + this.squares_diff(y1, d1);
+	  
+	        let discriminant = Math.pow(b*b - 4 * a * c, 0.5);
+	        
+	        y = (-b + discriminant) / (2 * a);
+			x = y * alpha + beta;
+			possible_poses.push([x, y]);
+
+	        y_ = y - discriminant / a;
+	        x_ = y_ * alpha + beta;
+	        possible_poses.push([x_, y_]) 
+        }
+        for (const coord of possible_poses){
+        	if ((Math.abs(coord[0]) <= x_bound) && (Math.abs(coord[1]) <= y_bound)){
+        		in_field.push(coord);
+        	}
+        }
+        if (in_field.length === 2 && e1){
+        	result = this.get_best(in_field[0], in_field[1], x1, y1, x2, y2, e1, e2);
+        } else {
+        	result = in_field[0];
         }
 
-        var alpha = (y1 - y2) / (x2 - x1);
-        var beta = (this.squares_diff(y2, y1) + this.squares_diff(x2, x1) + this.squares_diff(d1, d2)) / (2 * (x2 - x1));
-
-        var a = alpha * alpha + 1;
-        var b = -2 * (alpha * (x1 - beta) + y1);
-        var c = Math.pow(x1 - beta, 2) + this.squares_diff(y1, d1);
-  
-        var discriminant = Math.pow(b*b - 4 * a * c, 0.5);
-        y = (-b + discriminant) / (2 * a);
-
-        if (Math.abs(y) > y_bound){
-            y = y - discriminant / a;
-        }
-
-        x = y * alpha + beta;
-        if (Math.abs(x) > x_bound){
-            y = y - discriminant / a;
-            x = y * alpha + beta;
-        }
-        return [x, y];
+        return result;
     },
 
-    get_object_coords(d1, da, x, y, x1, y1, a1, aa){
-        a1 = a1 / Math.PI * 180;
-        aa = aa / Math.PI * 180;
-        d_a1 = d1 * d1 + da * da - 2 * d1 * da * Math.cos(Math.abs(a1 - aa));
-        return solve(da, x, y, d_a1, x1, y1)
+    get_object_coords(d1, da, x, y, x1, y1, a1, aa, eo){
+        let d_a1 = d1 * d1 + da * da - 2 * d1 * da * Math.cos(Math.abs(a1 - aa) * Math.PI / 180);
+        d_a1 = Math.pow(d_a1, 0.5);
+        eo = [eo[0] * -1, eo[1] * -1];
+        return this.solveby2(da, d_a1, x, y, x1, y1, eo, null, 57.5, 39);
     },
 
     find_different_x_y(flags, flag){
@@ -71,25 +107,86 @@ module.exports = {
         return null;
     },
 
-    get_flags(data){
-        var flags = [];
+    get_flags_and_objects(data){
+        let flags = [];
+        let objects = [];
+        let cur;
         for (const obj of data){
             if (typeof obj === 'number'){
                 continue;
             }
             obj_name = obj['cmd']['p'].join('');
-            if (!Flags[obj_name] || obj['p'].length === 1){
+            //console.log(obj_name);
+            if (obj['p'].length === 1){
                 continue;
             }
+
+            if (!Flags[obj_name]){
+                if (obj_name.includes("p")){
+                    //console.log(obj['cmd']['p']);
+                    objects.push([obj['p'][0], obj['p'][1]]);
+                }
+                continue;
+            }
+
             cur = [Flags[obj_name]['x'], Flags[obj_name]['y'], obj['p'][0], obj['p'][1]];
             flags.push(cur);
-            if (flags.length >= 2){ // Заменить на условие выхода по трем флагам; все 3 возвращаемых флага должны иметь разные координаты по x; 
-            	                    // если таких трех флагов не найдется, то возвратить любые 2 или 1 флага
-     			break;
-            }
         }
-        return flags;
+        //console.log("------------");
+        return [flags, objects];
     },
+
+    get_flags_and_objects_2(data){
+        let flags = [];
+        let objects = [];
+        let sortedFlags = {};
+        let cur;
+        let res = [];
+        for (const obj of data){
+            if (typeof obj === 'number'){
+                continue;
+            }
+            obj_name = obj['cmd']['p'].join('');
+
+            if (obj['p'].length === 1){
+                continue;
+            }
+
+            if (!Flags[obj_name]){
+                if (obj_name.includes("p")){
+                    objects.push([obj['p'][0], obj['p'][1]]);
+                }
+                continue;
+            }
+
+            cur = [Flags[obj_name]['x'], Flags[obj_name]['y'], obj['p'][0], obj['p'][1]];
+
+            if (res.length < 3){
+                if (!sortedFlags[cur[0]]) {
+                    sortedFlags[cur[0]] = [];
+                    sortedFlags[cur[0]].push(cur);
+                } else {
+                    sortedFlags[cur[0]].push(cur);
+                }
+
+                if (Object.keys(sortedFlags).length === 3) {
+                    for (let [key, value] of Object.entries(sortedFlags)) {
+                        res.push(value[0]);
+                        if (res.length === 3) {
+                            break;
+                        }
+                    }
+                }
+            }
+            flags.push(cur);
+        }
+        if (res.length === 3){
+            return [res, objects];
+        } else {
+            return [[flags[0], flags[1]], objects]; 
+        }
+    },
+
 }
 
 
