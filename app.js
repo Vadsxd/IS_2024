0d45d44abdd0d76a4b6b4af632a868dcbe8ae4af
vadim <vadim.sannikov.2018@mail.ru>
2024-04-01 11:20:21 +0300

    refactor

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-const readline = require('readline');
 const Agent = require('./agent');
 const Socket = require('./socket');
-const Controller = require('./controller');
-const Manager = require("./manager");
-const dt2 = require("./echelon2_dt2");
 const VERSION = 7;
 
 
 (async () => {
-    let assist_playerCords, score_playerCords, rotationSpeed, npc1Cords, npc2Cords;
+    let score_playerCords = [-20, 0];
+    let goalkeeper_coords = [-40, 0];
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    const it = rl[Symbol.asyncIterator]();
-
-    score_playerCords = [-20, 0];
-    goalkeeper_coords = [-40, 0];
-    /*
-    let score_player = new Agent('A');
-    score_player.playerName = "score_player";
-    score_player.dt = dt2;
-    score_player.manager = new Manager();
-    */
     let score_player = new Agent("A", false);
     let goalkeeper = new Agent("B", true);
 
@@ -32,6 +15,4 @@ const VERSION = 7;
 
     await score_player.socketSend('move', `${score_playerCords[0]} ${score_playerCords[1]}`);
     await goalkeeper.socketSend('move', `${goalkeeper_coords[0]} ${goalkeeper_coords[1]}`);
-
-
 })();
